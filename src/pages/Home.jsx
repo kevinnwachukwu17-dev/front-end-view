7d394b62a97126820c6fe717302876dcce2cb35e
kevinnwachukwu17-dev <kevinnwachukwu17@gmail.com>
2026-07-20 09:45:50 +0100

    Change made to ui-ux

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -23,7 +23,7 @@ import cybersecurityImg from '../assets/home_cards_imgs/cybersecurity.jpg';
 import digitalAnalyticsImg from '../assets/home_cards_imgs/digital-analytics.jpg';
 import ieltsImg from '../assets/home_cards_imgs/ielts-img.jpg';
 import celpipImg from '../assets/home_cards_imgs/celpip-img.jpg';
-import uxuiImg from '../assets/home_cards_imgs/uxui-img.jpg';
+import uxuiImg from '../assets/home_cards_imgs/ux-ui.jpg';
 
 // Import testimonial images - using fewer for performance
 import testimonialImg1 from '../assets/testimony_imgs/testimonyImg1.jpg';
--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -20,7 +20,7 @@ import cybersecurityImg from '../assets/home_cards_imgs/cybersecurity.jpg';
 import digitalAnalyticsImg from '../assets/home_cards_imgs/digital-analytics.jpg';
 import ieltsImg from '../assets/home_cards_imgs/ielts-img.jpg';
 import celpipImg from '../assets/home_cards_imgs/celpip-img.jpg';
-import uxuiImg from '../assets/home_cards_imgs/uxui-img.jpg';
+import uxuiImg from '../assets/home_cards_imgs/ux-ui.jpg';
 
 // Service Card Component
 const ServiceCard = ({ service, onWhatsApp, onPhoneCall, onRegister, onQuickEnquiry }) => {
